9758aca49fd2c10570d6ce4f8b5c0555d2209442
Lucas Padua <lucas.p2222@gmail.ciom>
2018-11-12 15:08:08 +1100

    OOP

--- oopsandbox/2_3.php
+++ oopsandbox/2_3.php
@@ -4,22 +4,31 @@
 
 // Define a Class
 class user {
-  // Properties(attributes)
+    // Properties(attributes)
 
-    public $name = 'Lucas';
+    // public $name = 'Lucas';
+    public $name;
 
-  // Methods
-
-  public function sayHello(){
-    return $this->name .'Says Hello';
+    // Methods
+    public function sayHello(){
+      return $this->name .' Says Hello';
   }
 }
 
 // Instatiate a user object from the user class
 
   $user1 = new User();
+
+  $user1->name = 'Lucas';
   echo $user1->name;
-
   echo '<br>';
-
   echo $user1->sayHello();
+///////////////////
+  echo '<br>';
+///////////////////
+  // Create new user
+  $user2 = new User();
+  $user2->name = 'Tiago';
+  echo $user2->name;
+  echo '<br>';
+  echo $user2->sayHello();
