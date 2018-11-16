e9707cbce8f00fb49b1032abc680ad570466cbcb
Lucas Padua <lucas.p2222@gmail.ciom>
2018-11-16 13:53:02 +1100

    MVC almost done

--- sharePostsapp/app/bootstrap.php
+++ sharePostsapp/app/bootstrap.php
@@ -1,1 +1,14 @@
-Hello Boot
+<?php
+  //load config
+  require_once 'config/config.php';
+
+  // // Load Libraries
+  // require_once 'libraries/core.php';
+  // require_once 'libraries/controller.php';
+  // require_once 'libraries/database.php';
+
+  // Autoload Core Libraries
+
+  spl_autoload_register(function($className){
+    require_once 'libraries/' . $className . '.php';
+  });
---
+++ sharePostsapp/app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+  // App Root
+  // echo dirname(dirname(__FILE__));
+
+
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/phpsandbox/sharepostsapp');
+  // Site Name
+  define('SITENAME', 'Share Posts');
