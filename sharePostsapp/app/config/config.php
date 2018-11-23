0dea4dd5814ea2492dc6cfd0bdfb13ba2db1f964
Lucas Padua <lucas.p2222@gmail.ciom>
2018-11-23 12:46:24 +1100

    Reset PHP ADMIN

--- sharePostsapp/app/config/config.php
+++ sharePostsapp/app/config/config.php
@@ -1,7 +1,12 @@
 <?php
   // App Root
   // echo dirname(dirname(__FILE__));
+  //db params
 
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '123456');
+  // define('DB_NAME', 'phpblog');
 
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
---
+++ sharePostsapp/app/libraries/database.php
@@ -0,0 +1,37 @@
+/*<?php
+/*
+  -PDO Database class
+  - Connect To database
+  - Create prepared Statements
+  - Bind values
+  - Return rows and results
+*/
+
+class Database {
+  private $host = DB_HOST;
+  private $user = DB_USER;
+  private $pass = BD_PASS;
+  // private $dbname = DB_NAME;
+
+
+  private $dbh;
+  private $stmt;
+  private $error;
+
+  public function _construct(){
+    //Set DSN
+    $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+    $options = array(
+      PDO::ATTR_PERSISTENT => true,
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+    //Create PDO instance
+    try{
+      $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+    } catch(PDOException $e){
+      $this->error = $e->getMessage();
+      echo $this->error;
+    }
+
+  }
+}
