70f70d79b67d2c67dc62f56aa91cdbc72f89057f
Lucas Padua <lucas.p2222@gmail.ciom>
2018-12-04 08:52:42 +1100

    MVC Clean for new Projects

--- sharePostsapp/app/config/config.php
+++ sharePostsapp/app/config/config.php
@@ -1,15 +1,11 @@
 <?php
-  // App Root
-  // echo dirname(dirname(__FILE__));
-  //db params
-
   define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_PASS', '123456');
-  define('DB_NAME', 'sharepost');
+  define('DB_USER', '_PROJECT_USER_');
+  define('DB_PASS', '_PROJECT_PASS_');
+  define('DB_NAME', '_YOUR_DBNAME_');
 
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/phpsandbox/sharepostsapp');
+  define('URLROOT', '_YOUR_URL_');
   // Site Name
-  define('SITENAME', 'Share Posts');
+  define('SITENAME', '_YOUR_SITE_NAME_');
